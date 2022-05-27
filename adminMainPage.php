9618a536a929c1e16beaea4108925be7496a57da
Tanchy101 <angelo.tancioco@gmail.com>
2022-05-27 12:55:23 +0800

    added adminMainPage
    this is the main page of admin user to put CRUD products and etc

---
+++ adminMainPage.php
@@ -0,0 +1,23 @@
+<?php 
+include 'Config.php'
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            .error{
+                color:red;
+            }
+            .success{
+                color:green;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Admin</h1>
+        <a href = "adminChangePass.php">Change password</a>
+        <h3>Welcome to the admin main page!</h3>
+    </body>
+</html>
