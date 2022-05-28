4c0076fc570073844b4a44545a560fd4e6a77565
Jolz <95854722+Angelojinio@users.noreply.github.com>
2022-05-28 10:00:10 +0800

    comment the php code
    sa mga mag eedit try nyo muna i comment yung codes ng php para makita nyo yung result for html at hindi mag fatal error , hinahanap kase nung data base at mga tables eh , since html naman ilalagay ganon nalang muna , thanks

--- userHomePage.php
+++ userHomePage.php
@@ -1,22 +1,22 @@
 <?php
 
-//     $host = "localhost";
-//     $dbusername = "root";
-//     $dbpassword = "";
-//     $databaseName = "loginpage";
-//     $port = 3306;
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $databaseName = "loginpage";
+    $port = 3306;
 
-//     // Create connection
-//     $conn = new mysqli($host, $dbusername, $dbpassword, $databaseName, $port);
+    // Create connection
+    $conn = new mysqli($host, $dbusername, $dbpassword, $databaseName, $port);
 
-//     // Check connection
-//     if ($conn->connect_error) {
-//         die("Connection failed: " . $conn->connect_error);
-//     }
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-//     // Selecting / Reading Queery
-// $sql = "SELECT * FROM `login`";
-// $result = $conn->query($sql);
+    // Selecting / Reading Queery
+$sql = "SELECT * FROM `login`";
+$result = $conn->query($sql);
 
 
 ?>
@@ -44,16 +44,16 @@
 
 
     <body>
-        <?php //if ($result->num_rows > 0) {
-        //     // output data of each row
-        //     while($row = $result->fetch_assoc()) {
-        //         echo "<h1>Welcome " . $row["email"] . "</h1>"; 
-        //     }
-        // } else {
-        //     echo "0 results";
-        // }
-        // $conn->close();
-        // ?>
+        <?php if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<h1>Welcome " . $row["email"] . "</h1>"; 
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
          <a href="logout.php">Logout</a> 
         <!-- pwede ka na mag lagay dito sa ilalim ng comment ko na to na mga need
         na ilagay sa home page-->
