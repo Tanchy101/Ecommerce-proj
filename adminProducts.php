caa7f425d7c16739e059fde798c294c86fc78232
harlenex <hbmedrano2021@plm.edu.ph>
2022-05-29 17:16:30 +0800

    added list of products [table]

--- adminProducts.php
+++ adminProducts.php
@@ -5,9 +5,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <h1> The Paper Bag. </h1>
+    <h3> List of Products </h3>
 </head>
 
 <body>
+    <table> 
+        <tr>
+            <th> Categories </th>
+            <th> Products </th>
+            <th> Variations </th>
+            <th> Price </th>
+        </tr>
+        <tr>
+            <td> Papers </td>
+            <td> Bond Paper </td>
+            <td> Size </td>
+            <td> 00.00 </td>
+        </tr>
+    </table>
 </body>
 
 </html>
