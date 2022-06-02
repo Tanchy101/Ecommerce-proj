4aa89aa923f92b12cc91d73aca91d0266080106a
Fritsss <fritssumaculub@gmail.com>
2022-06-02 23:49:43 +0800

    Added form
    not the actual

--- adminEditProducts.php
+++ adminEditProducts.php
@@ -9,5 +9,25 @@ $port = 3306;
 
 <!DOCTYPE html>
 <html>
+    <head>
+    </head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <label for = "products">Product</label>
+        <input type="text" name="products" placeholder="Enter the product" required>
+        <br>
+        <br>
+        <label for = "price">New Price</label>
+        <input type="number" name="price" required>
+        <br>
+        <br>
+        <label for = "quantity">New Quantity</label>
+        <input type="number" name="quantity" required> 
+        <br>
+        <br>
+        <input type="submit" value="SAVE">
+
+    </form>
+</body>
 
 </html>
