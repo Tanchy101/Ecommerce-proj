44b81a3b3e27bc1fda637b8ea12080907a1d22c4
Gene Pahoyo <genedaniela019@gmail.com>
2022-06-12 10:56:23 +0800

    Added Footer

--- addtoCart.php
+++ addtoCart.php
@@ -203,12 +203,6 @@ if(isset($_POST['order'])){
         a.active {
             color: #ffcbb5;
             }
-        .footer {
-            padding: 20px;
-            margin-top: 20px;
-            background-color: #ffcbb5;
-            text-align: center;
-            }
         table {
          border-collapse: collapse;
          width: 80%;
@@ -219,6 +213,52 @@ if(isset($_POST['order'])){
         text-align: left;
         padding: 8px;
             }
+
+     footer {
+            width: 100%;
+            bottom: 0;
+            background: linear-gradient(to right, #d3a35d, #ffcbb5);
+            padding: 100px 0 30px;
+            border-top-left-radius: 125px;
+            border-top-right-radius: 125px;
+            font-size: 13px;
+            line-height: 5px;
+            left: 0;
+           
+
+            }
+            .row {
+            width: 85%;
+            margin: auto;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: flex-start;
+            justify-content: space-between;
+            }
+            .col {
+            flex-basis: 27%; 
+            padding: 10px;
+            }
+            .logo {
+            height: 140px;
+            width: 160px;
+            margin-bottom: 30px;
+            }
+            .col h3 {
+            width: fit-content;
+            margin-bottom: 40px;
+            position: relative;
+             }
+             .col ul li {
+            list-style: none;
+            margin-bottom: 12px;
+             }
+             .col ul li a {
+            text-decoration: none;
+            color: #000000;
+            }
+
+        
     </style>
     <body style = "background-color: #ffedc0">
         <?php //if ($result->num_rows > 0) {
@@ -332,14 +372,33 @@ if(isset($_POST['order'])){
            
         </table>
         </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        
+        <footer>
+            <div class = "row">
+                <div class = "col">
+                    <!--logo-->
+                    <img src = "https://i.imgur.com/EKjxLuY.png" alt = "the paper bag logo"  class = "logo">
+                 </div>
+                 <div class = "col">
+                    <center> <h3> Follow us on </h3> </center>
+                    <ul>
+                         <a href = "https://www.facebook.com/?stype=lo&jlou=Afdo9_8IzKjd-98S53hgWcs_YTL09G0gr2QFRljr_iv46_YAcls5iVZeqmHpGZC539as2z3YZrVmDMN4Fa7qZwlkDHYfPePzF_auNbBsMVT-8g&smuh=35351&lh=Ac-aDteK0xAi75BCmxY"><img src = "https://i.imgur.com/juyHCD8.png" alt = "fb" width = "70" height = "70"></a> 
+                         <a href = "https://www.instagram.com/accounts/login/"><img src = "https://i.imgur.com/VoN7z9i.png" alt = "ig" width = "70" height = "70"></a> 
+                         <a href = "https://twitter.com/"><img src = "https://i.imgur.com/yWnTdsy.png" alt = "twt" width = "70" height = "70"></a>
 
-    
-       
-
-
-            
-       <div class = "footer">
-        footer.
-       </div>
+                         <p><img src = "https://i.imgur.com/QacTXH9.png" alt = "email" width = "25" height = "25"></a> thepaperbag_mnl@gmail.com <a href = "#"> </p>
+                         <p><img src = "https://i.imgur.com/QacTXH9.png" alt = "phone" width = "25" height = "25"></a> (+63) 930 7329 433</p>
+                    </ul>
+                </div>
+                <div class = "col">
+                    <h3> Working Hours </h3>
+                    <p> Monday - Saturday</p>
+                    <p> 8:00 AM - 10:00 PM</p>
+                </div>
+             </div>
+           <hr>
+             <center> <p><i> Copryright &copy; 2022 - The Paper Bag.All Right Reserved. </i></p> </center> 
+        </footer>
 </body>
 </html>
