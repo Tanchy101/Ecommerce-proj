3bc9c63dc95465f9819f7107f6e1e652b29e50b4
Fritsss <fritssumaculub@gmail.com>
2022-06-25 17:49:13 +0800

    add hover and footer

--- userPurchases.php
+++ userPurchases.php
@@ -161,6 +161,7 @@ if($result->num_rows > 0){
         body {
             font-family: monospace;
             margin: 25px;
+        
         }
         a:link {
             color: #000000;
@@ -204,18 +205,64 @@ if($result->num_rows > 0){
             .tableC{
                 align-items: center ;
             }
+            footer {
+            width: 100%;
+            bottom: 0;
+            background: linear-gradient(to right, #d3a35d, #ffcbb5);
+            padding: 100px 0 30px;
+            border-top-left-radius: 125px;
+            border-top-right-radius: 125px;
+            font-size: 13px;
+            line-height: 5px;
+            left: 0;
+           
+            }
+        .row {
+            width: 85%;
+            margin: auto;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: flex-start;
+            justify-content: space-between;
+            }
+        .col {
+            flex-basis: 27%; 
+            padding: 10px;
+            }
+        .logo {
+            height: 140px;
+            width: 160px;
+            margin-bottom: 30px;
+            }
+        .col h3 {
+            width: fit-content;
+            margin-bottom: 40px;
+            position: relative;
+             }
+        .col ul li {
+            list-style: none;
+            margin-bottom: 12px;
+             }
+        .col ul li a {
+            text-decoration: none;
+            color: #000000;
+        }
+        .topnavclick:hover {
+                -webkit-filter: drop-shadow(3px 3px 3px #B28256);
+                filter: drop-shadow(3px 3px 3px #B28256); 
+            }
 </style>
 
 <body style = "background-color: #ffedc0">
         
-        
+     <center>   
         <div class="topnav">
             <br>
             <h2>The Paper Bag.</h2>
-            <a href="logoutFileForUsers.php"><img src="https://i.imgur.com/Ua6SIs7.png" alt="Cart"width="35" height="30"></a>
-            <a href="addtoCart.php"><img src="https://i.imgur.com/izpY4HG.png" alt="Cart"width="30" height="30"></a>
-            <a href="profile.php"><img src="https://i.imgur.com/9Sd1au3.png" alt="Cart"width="35" height="30"></a>
-            <a href="userHomePage.php"><img src="https://i.imgur.com/hVZsoCl.png" alt="Cart"width="35" height="30"></a>
+            <a class="topnavclick" href="logoutFileForUsers.php"><img src="https://i.imgur.com/Ua6SIs7.png" alt="Cart"width="35" height="30"></a>
+            <a class="topnavclick" href="addtoCart.php"><img src="https://i.imgur.com/izpY4HG.png" alt="Cart"width="30" height="30"></a>
+            <a class="topnavclick" href="profile.php"><img src="https://i.imgur.com/9Sd1au3.png" alt="Cart"width="35" height="30"></a>
+            <a class="topnavclick" href="userHomePage.php"><img src="https://i.imgur.com/hVZsoCl.png" alt="Cart"width="35" height="30"></a>
            
             <div class="wrap">
                 <div class="search">
@@ -295,6 +342,41 @@ if($result->num_rows > 0){
 
     </table>
     </fieldset>
+    </center>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <footer>
+            <div class = "row">
+                <div class = "col">
+                    <!--logo-->
+                    <img src = "https://i.imgur.com/EKjxLuY.png" alt = "the paper bag logo"  class = "logo">
+                 </div>
+                 <div class = "col">
+                    <center> <h3> Follow us on </h3> </center>
+                    <ul>
+                         <a href = "https://www.facebook.com/?stype=lo&jlou=Afdo9_8IzKjd-98S53hgWcs_YTL09G0gr2QFRljr_iv46_YAcls5iVZeqmHpGZC539as2z3YZrVmDMN4Fa7qZwlkDHYfPePzF_auNbBsMVT-8g&smuh=35351&lh=Ac-aDteK0xAi75BCmxY"><img src = "https://i.imgur.com/juyHCD8.png" alt = "fb" width = "70" height = "70"></a> 
+                         <a href = "https://www.instagram.com/accounts/login/"><img src = "https://i.imgur.com/VoN7z9i.png" alt = "ig" width = "70" height = "70"></a> 
+                         <a href = "https://twitter.com/"><img src = "https://i.imgur.com/yWnTdsy.png" alt = "twt" width = "70" height = "70"></a>
+
+                         <p><img src = "https://i.imgur.com/QacTXH9.png" alt = "email" width = "25" height = "25"></a> thepaperbag_mnl@gmail.com <a href = "#"> </p>
+                         <p><img src = "https://i.imgur.com/QacTXH9.png" alt = "phone" width = "25" height = "25"></a> (+63) 930 7329 433</p>
+                    </ul>
+                </div>
+                <div class = "col">
+                    <h3> Working Hours </h3>
+                    <p> Monday - Saturday</p>
+                    <p> 8:00 AM - 10:00 PM</p>
+                </div>
+             </div>
+           <hr>
+             <center> <p><i> Copryright &copy; 2022 - The Paper Bag.All Right Reserved. </i></p> </center> 
+        </footer>
 <div>
         <?php
         }
