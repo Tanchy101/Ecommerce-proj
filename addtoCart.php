6b603d495e2158c161877948d58f65ce4cde6166
harlenex <hbmedrano2021@plm.edu.ph>
2022-08-28 21:22:57 +0800

    Update addtoCart.php

--- addtoCart.php
+++ addtoCart.php
@@ -501,7 +501,6 @@ if(isset($_POST['order'])){
              </div>
            <hr>
              <center> <p><i> Copryright &copy; 2022 - The Paper Bag.All Right Reserved. </i></p> </center> 
-             <center> hello goodbye! </center>
         </footer>
 </body>
 </html>
